261d4356ed2539145339c03a985226ce01c9dc80
Alvaro Lupa <alvaro.lupa@gmail.com>
2020-05-28 12:29:14 -0400

    Configuración de plantilla para el backend

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(User $user)
     {
-        return view('dashboard');
+        return view('dashboard', [
+            'users' => $user->take(5)->get()
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,14 @@
 @extends('layouts.app')
 
+@section('title', 'Resumen')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Panel administrativo</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <ul class="list-group">
+        @foreach($users as $user)
+            <li class="list-group-item">
+                <h4>{{ $user->name }}</h4>
+                <em>Último ingreso: ---</em>
+            </li>
+        @endforeach
+    </ul>
 @endsection
