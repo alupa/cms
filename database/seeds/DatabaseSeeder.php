872a12486d0d90644fecb56bb415527fc36cd0e9
Alvaro Lupa <alvaro.lupa@gmail.com>
2020-05-28 11:23:08 -0400

    Configuración inicial para el frontend

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        App\Page::create([
+            'parent_id' => null,
+            'title'     => 'Quienes somos',
+            'slug'      => 'quienes-somos',
+            'body'      => 'Contenido de quienes somos'
+        ]);
+
+        App\Page::create([
+            'parent_id' => 1,
+            'title'     => 'Misión',
+            'slug'      => 'mision',
+            'body'      => 'Contenido de misión'
+        ]);
+        
+        App\Page::create([
+            'parent_id' => 1,
+            'title'     => 'Visión',
+            'slug'      => 'vision',
+            'body'      => 'Contenido de visión'
+        ]);
     }
 }
